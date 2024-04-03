4f6794847181dc25e4d9584fda7e6f3293aee63e
Prem Prakash Shinde <shindeprem102@gmail.com>
2024-04-04 00:32:12 +0530

    trying to deploy PO1 Module

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,66 +1,9 @@
-"use client";
-import { title } from "@/components/primitives";
-import {
-	Table,
-	TableHeader,
-	TableColumn,
-	TableBody,
-	TableRow,
-	TableCell,
-} from "@nextui-org/react";
-import { useEffect, useState } from "react";
+import React from "react";
 
-import * as XLSX from "xlsx";
-import { saveAs } from "file-saver";
-import { Button } from "@nextui-org/button";
+type Props = {};
 
-export default function PricingPage() {
-	const [tableData, setTableData] = useState<any[][]>([]);
+const page = (props: Props) => {
+	return <div>pricing</div>;
+};
 
-	useEffect(() => {
-		// Fetch your table data here or set it directly
-		const data = [
-			["NAME", "ROLE", "STATUS"],
-			["Tony Reichert", "CEO", "Active"],
-			["Zoey Lang", "Technical Lead", "Paused"],
-			["Jane Fisher", "Senior Developer", "Active"],
-			["William Howard", "Community Manager", "Vacation"],
-		];
-		setTableData(data);
-	}, []);
-
-	const exportToExcel = () => {
-		const wb = XLSX.utils.book_new();
-		const ws = XLSX.utils.aoa_to_sheet(tableData);
-		XLSX.utils.book_append_sheet(wb, ws, "Sheet1");
-		const excelBuffer = XLSX.write(wb, { bookType: "xlsx", type: "array" });
-		saveAs(
-			new Blob([excelBuffer], { type: "application/octet-stream" }),
-			"table.xlsx"
-		);
-	};
-
-	return (
-		<div>
-			<table aria-label='Example static collection table'>
-				<thead>
-					<tr>
-						<th>NAME</th>
-						<th>ROLE</th>
-						<th>STATUS</th>
-					</tr>
-				</thead>
-				<tbody>
-					{tableData.map((row, rowIndex) => (
-						<tr key={rowIndex}>
-							{row.map((cell, cellIndex) => (
-								<td key={`${rowIndex}-${cellIndex}`}>{cell}</td>
-							))}
-						</tr>
-					))}
-				</tbody>
-			</table>
-			<Button onClick={exportToExcel}>Download as Excel</Button>
-		</div>
-	);
-}
+export default page;
