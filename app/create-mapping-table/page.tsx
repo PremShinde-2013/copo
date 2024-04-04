62898056df0eedf3766203d9f35d9cb2da5ca8a9
Prem Prakash Shinde <shindeprem102@gmail.com>
2024-04-04 19:35:00 +0530

    PO2  Completed completely till download  for deployment

--- app/create-mapping-table/page.tsx
+++ app/create-mapping-table/page.tsx
@@ -850,7 +850,8 @@ export default function App() {
 		// }
 
 		try {
-			const res = await fetch("http://localhost:3000/api/table", {
+			// const res = await fetch("http://localhost:3000/api/table", {
+			const res = await fetch("https://copo-attainment.vercel.app/api/table", {
 				method: "POST",
 				headers: {
 					"Content-type": "application/json",
